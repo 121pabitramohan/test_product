a8161d3f2281d11c5e493746522d095cfca94542
121pabitramohan <121pabitramohan@gmail.com>
2024-05-25 23:47:39 +0500

    new

---
+++ demo/Studunt.java
@@ -0,0 +1,10 @@
+package demo;
+
+import java.util.List;
+
+public class Studunt {
+    public static void main(String[] args) {
+        List<String> list= List.of("'a','bca','b'");
+        System.out.println("hi, how are you?");
+    }
+}
